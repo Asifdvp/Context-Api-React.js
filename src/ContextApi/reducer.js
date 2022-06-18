c2a40a98aec3e6a9384bb7b0071311290bfb127c
Asif Mustafayev <53805470+Asifdvp@users.noreply.github.com>
2022-06-18 15:57:27 +0400

    Complete Context-api

--- src/ContextApi/reducer.js
+++ src/ContextApi/reducer.js
@@ -1,11 +1,10 @@
-import { initialState } from "./store";
 
-export function reducer(state=initialState,aciton){
+export function reducer(state,aciton){
     switch(aciton.type){
         case "INCREASE_COUNTER":
-            state.counter = state.counter +1;
-            console.log(state.counter)
-            return {...state}
+            state.counter +=1;
+            console.log(state)
+            return  {...state} 
      case "DECREASE_COUNTER":
      state.counter = state.counter -1
       return {...state}
